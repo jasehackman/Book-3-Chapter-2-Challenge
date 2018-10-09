a9ff8e6606fdb0c51b2ab0462bfe1c035e9c2c15
jasehackman <jasehackman@gmail.com>
2018-10-09 15:49:07 -0500

    created and finished the challenge

---
+++ main.js
@@ -0,0 +1,48 @@
+const eliz = {
+  name: "Elizabeth Sanger",
+  district: 205,
+  platforms: [{
+    taxes: "lower taxes",
+    jobs: "create more jobs",
+    infrastructure: "more infrastructure",
+    heathCare: "better health care",
+    crime: "less crime"
+  }
+  ],
+  donationForm: "givemeallyourmoney.gov",
+  cal: [{
+    eventName: "Kiss all the babies",
+    startTime: "10:00am",
+    endTime: "3:00pm",
+    location: "near the babies"
+  }],
+  bio: "she is a really nice lady"
+  images: [{
+      headShot: "url",
+      family: "url",
+      constituents: "url"
+  }],
+  mission: "rule the world",
+  vote: "url"
+}
+
+const vol = {
+  name: "Fred",
+  address: [{
+    street: "111 South Bend St.",
+    city: "Nashville",
+    state: "TN",
+    zip: "37201"
+  }],
+  email: "email",
+  phone: "334-234-2343",
+  availability: [{
+    mon: "10:00am - 10:00pm",
+    tue: "10:00am - 10:00pm",
+    wed: "10:00am - 10:00pm",
+    thur: "10:00am - 10:00pm",
+    fri: "10:00am - 10:00pm",
+    sat: "10:00am - 10:00pm",
+    sun: "10:00am - 10:00pm"
+  }]
+}
